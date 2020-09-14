74beabc450c70a27f0fb2659738fad5abd105abd
IQBALARRAFI6706204091 <iqbalarrafi@student.telkomuniversity.ac.id>
2020-09-14 11:14:23 +0700

    HelloWorld.java

---
+++ Jurnal-02/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Nama : Iqbal Arrafi"); 
+        System.out.println("NIM : 6706204091");
+		System.out.println("Kelas : D3IF4403");
+    }
+}
